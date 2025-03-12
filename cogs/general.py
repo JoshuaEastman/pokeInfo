3c4aaaeacf52ddc4cfdfd8652b93343a3ddfe63c
Joshua Eastman <joshuabeastman@gmail.com>
2025-03-12 14:07:10 -0500

    removes clear command to reduce any explotation or permission based commands

--- cogs/general.py
+++ cogs/general.py
@@ -9,10 +9,5 @@ class General(commands.Cog):
     async def ping(self, ctx):
         await ctx.send('Pong!')
 
-    @commands.command(name='clear')
-    @commands.has_role('Dev 💎') # Restrict to dev role
-    async def clear(self, ctx, arg=10):
-        await ctx.channel.purge(limit=arg)
-
 async def setup(bot):
     await bot.add_cog(General(bot))
