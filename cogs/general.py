644db4cb310386edaf6eede448d32932128523f7
Joshua Eastman <joshuabeastman@gmail.com>
2025-03-13 22:29:22 -0500

    comments out embed testing commands

--- cogs/general.py
+++ cogs/general.py
@@ -20,67 +20,67 @@ class General(commands.Cog):
         coin = random.choice(['Heads', 'Tails'])
         await ctx.send(f'{coin}!')
 
-    @commands.command(name='embed_buttons', help='Sends an embed with multiple buttons')
-    async def embed_buttons(self, ctx):
-        # Initial embed message with instructions
-        embed = discord.Embed(
-            title="Choose an Embed",
-            description="Click on a button below to see a different embed.",
-            color=discord.Color.green()
-        )
+    # @commands.command(name='embed_buttons', help='Sends an embed with multiple buttons')
+    # async def embed_buttons(self, ctx):
+    #     # Initial embed message with instructions
+    #     embed = discord.Embed(
+    #         title="Choose an Embed",
+    #         description="Click on a button below to see a different embed.",
+    #         color=discord.Color.green()
+    #     )
 
-        # Create initial view with three buttons
-        view = EmbedButtonsView()
+    #     # Create initial view with three buttons
+    #     view = EmbedButtonsView()
 
-        # Send the initial embed with the buttons
-        await ctx.send(embed=embed, view=view)
+    #     # Send the initial embed with the buttons
+    #     await ctx.send(embed=embed, view=view)
 
 
-class EmbedButtonsView(View):
-    def __init__(self):
-        super().__init__()
+# class EmbedButtonsView(View):
+#     def __init__(self):
+#         super().__init__()
 
-    # Button 1: Displays Embed 1
-    @discord.ui.button(label="Embed 1", style=discord.ButtonStyle.primary, custom_id="embed_1")
-    async def embed_1(self, interaction: discord.Interaction, button: discord.ui.Button):
-        # Acknowledge the interaction
-        await interaction.response.defer()
+#     # Button 1: Displays Embed 1
+#     @discord.ui.button(label="Embed 1", style=discord.ButtonStyle.primary, custom_id="embed_1")
+#     async def embed_1(self, interaction: discord.Interaction, button: discord.ui.Button):
+#         # Acknowledge the interaction
+#         await interaction.response.defer()
 
-        # Create and update the message with Embed 1
-        embed = discord.Embed(
-            title="Embed 1",
-            description="This is the first embed.",
-            color=discord.Color.red()
-        )
-        await interaction.message.edit(embed=embed, view=self)
+#         # Create and update the message with Embed 1
+#         embed = discord.Embed(
+#             title="Embed 1",
+#             description="This is the first embed.",
+#             color=discord.Color.red()
+#         )
+#         await interaction.message.edit(embed=embed, view=self)
 
-    # Button 2: Displays Embed 2
-    @discord.ui.button(label="Embed 2", style=discord.ButtonStyle.primary, custom_id="embed_2")
-    async def embed_2(self, interaction: discord.Interaction, button: discord.ui.Button):
-        # Acknowledge the interaction
-        await interaction.response.defer()
+#     # Button 2: Displays Embed 2
+#     @discord.ui.button(label="Embed 2", style=discord.ButtonStyle.primary, custom_id="embed_2")
+#     async def embed_2(self, interaction: discord.Interaction, button: discord.ui.Button):
+#         # Acknowledge the interaction
+#         await interaction.response.defer()
 
-        # Create and update the message with Embed 2
-        embed = discord.Embed(
-            title="Embed 2",
-            description="This is the second embed.",
-            color=discord.Color.blue()
-        )
-        await interaction.message.edit(embed=embed, view=self)
+#         # Create and update the message with Embed 2
+#         embed = discord.Embed(
+#             title="Embed 2",
+#             description="This is the second embed.",
+#             color=discord.Color.blue()
+#         )
+#         await interaction.message.edit(embed=embed, view=self)
 
-    # Button 3: Displays Embed 3
-    @discord.ui.button(label="Embed 3", style=discord.ButtonStyle.primary, custom_id="embed_3")
-    async def embed_3(self, interaction: discord.Interaction, button: discord.ui.Button):
-        # Acknowledge the interaction
-        await interaction.response.defer()
+#     # Button 3: Displays Embed 3
+#     @discord.ui.button(label="Embed 3", style=discord.ButtonStyle.primary, custom_id="embed_3")
+#     async def embed_3(self, interaction: discord.Interaction, button: discord.ui.Button):
+#         # Acknowledge the interaction
+#         await interaction.response.defer()
 
-        # Create and update the message with Embed 3
-        embed = discord.Embed(
-            title="Embed 3",
-            description="This is the third embed.",
-            color=discord.Color.green()
-        )
-        await interaction.message.edit(embed=embed, view=self)
+#         # Create and update the message with Embed 3
+#         embed = discord.Embed(
+#             title="Embed 3",
+#             description="This is the third embed.",
+#             color=discord.Color.green()
+#         )
+#         await interaction.message.edit(embed=embed, view=self)
 
 async def setup(bot):
     await bot.add_cog(General(bot))
