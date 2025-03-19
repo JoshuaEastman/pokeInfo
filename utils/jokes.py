763b152e3856e84cc395e39a6057e7942285afe3
Joshua Eastman <joshuabeastman@gmail.com>
2025-03-19 17:49:35 -0500

    removes unused import on infoutil, creates option to pick a what type of joke

--- utils/jokes.py
+++ utils/jokes.py
@@ -1,16 +1,28 @@
 import aiohttp
-import discord
 
 JOKE_API_URL = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
 
-async def get_joke():
+async def get_joke(type=None):
+    """Fetch a joke from the JokeAPI"""
     async with aiohttp.ClientSession() as session:
-        async with session.get(JOKE_API_URL) as response:
-            if response.status == 200:
-                joke_data = await response.json()
-                if joke_data["type"] == "single":
+        if type == "single":
+            async with session.get(f"{JOKE_API_URL}&type=single") as response:
+                if response.status == 200:
+                    joke_data = await response.json()
                     return joke_data["joke"]
                 else:
+                    return "Failed to fetch a joke. Please try again later."
+        elif type == "double":
+            async with session.get(f"{JOKE_API_URL}&type=twopart") as response: 
+                if response.status == 200:
+                    joke_data = await response.json()
                     return f"{joke_data['setup']}\n{joke_data['delivery']}"
-            else:
-                return "Failed to fetch a joke. Please try again later."
+                else:
+                    return "Failed to fetch a joke. Please try again later."
+        else:
+            async with session.get(f"{JOKE_API_URL}") as response:
+                if response.status == 200:
+                    joke_data = await response.json()
+                    return f"{joke_data['setup']}\n{joke_data['delivery']}"
+                else:
+                    return "Failed to fetch a joke. Please try again later."
--- utils/pokemonInfoUtil.py
+++ utils/pokemonInfoUtil.py
@@ -1,5 +1,4 @@
 from PIL import Image, UnidentifiedImageError
-import discord
 import aiohttp
 import io
 import re
