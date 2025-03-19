5fe9754ae1c7005051354afc74176072e3fbc574
Joshua Eastman <joshuabeastman@gmail.com>
2025-03-18 22:49:12 -0500

    does the same darn thing again

--- cogs/pokemonStats.py
+++ cogs/pokemonStats.py
@@ -16,7 +16,9 @@ class PokemonStats(commands.Cog):
                 self.channel_id = int(os.getenv('PERSONAL_CHANNEL_ID'))
         else:
             if os.environ['BOT_ENV'] == 'production':
-                self.channel_id = int(os.environ['CMDS_CHANNEL_ID'])
+                channel_value = os.environ['CMDS_CHANNEL_ID']
+                clean_channel_value = channel_value.strip('"')
+                self.channel_id = int(clean_channel_value)
 
         # Command to get the base stats of a Pokemon
     @commands.command(name="pokestats", help=" - Enter ~pokestats and name of a pokemon to get the base stats for that pokemon. Example: ~pokestats pikachu.")
